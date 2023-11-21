630dbdef212a1a23324ac8dd714a8b1fe65a14d2
Alberto Moura <palbertorm35@gmail.com>
2023-11-21 02:10:17 +0100

    nivel2

--- S1_02_N2_Exe1/src/Entrada.java
+++ S1_02_N2_Exe1/src/Entrada.java
@@ -1,9 +1,90 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
-class Entrada extends Scanner {
-    public Entrada(){
-        super(System.in);
+public class Entrada {
+   static Scanner entrada = new Scanner(System.in);
+
+   public static byte leerByte(String mensaje) {
+       byte data = 0;
+       boolean validacion = false;
+
+           try
+               {
+                   System.out.println(mensaje);
+                   data = entrada.nextByte();
+                   validacion = true;
+               }
+           catch (InputMismatchException e){
+               System.err.println("InputMismatchException format" + e.getMessage());
+           }
+       return data;
+   }
+   public static int leerInt(String mensaje) {
+       int data = 0;
+       boolean validacion = false;
+           try
+               {
+                   System.out.println(mensaje);
+                   data = entrada.nextInt();
+                   validacion = true;
+               }
+           catch (InputMismatchException e){
+               System.err.println("InputMismatchException format" + e.getMessage());
+           }
+       return data;
+   }
+   public static float leerFloat(String mensaje) {
+       float data = 0;
+       boolean validacion = false;
+
+       try {
+           System.out.println(mensaje);
+           data = entrada.nextFloat();
+           validacion = true;
+       }catch (InputMismatchException e) {
+           System.err.println("InputMismatchException format" + e.getMessage());
+       }
+       return data;
+   }
+   public static double leerDouble(String mensaje) {
+       double data = 0.0;
+       boolean validacion = false;
+
+       try {
+           System.out.println(mensaje);
+           data = entrada.nextDouble();
+           validacion = true;
+       }catch (InputMismatchException e) {
+           System.err.println("InputMismatchException format" + e.getMessage());
+       }
+       return data;
+   }
+
+   //Mètodes a implantar capturant l’excepció de la classe Exception:
+
+    public static char leerChar(String mensaje) throws InputInvalidException {
+       char data;
+       boolean validacion = false;
+
+       try {
+           System.out.println(mensaje);
+           data = (char) entrada.nextShort();
+           validacion = true;
+       }catch (Exception e){
+           throw new InputInvalidException("Error de formato");
+       }
+    return data;
     }
-    public int leerInt(String mensaje) throws InputInvalidException {
-        return 0;
+    public static String leerString(String mensaje) throws InputInvalidException {
+       String data;
+       boolean validacion = false;
+
+       try {
+           System.out.println(mensaje);
+           data = entrada.nextLine();
+           validacion = true;
+       }catch (Exception e){
+           throw new InputInvalidException("Error de formato");
+       }
+       return data;
     }
 }
--- S1_02_N2_Exe1/src/Main.java
+++ S1_02_N2_Exe1/src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String mensaje = "Introduce tu edad";
+        System.out.println(" entra tu edad");
+        byte edad = Entrada.leerByte(mensaje);
+        //System.out.println(edad);
     }
 }
+/*Crea una clase llamada "Entrada". Esta clase debe servir para controlar las distintas excepciones que pueden aparecer en Java a la hora de introducir datos por teclado utilizando la clase Scanner.
+
+Lo primero que debe hacerse es instanciar un objeto de la clase Scanner ya partir de ahí, crear métodos estáticos para leer los diferentes tipos de datos desde el teclado.
+
+Importante
+
+Es importante que si en algún método salta una excepción, la tratamos y volvemos a pedir el dato al usuario hasta que éste esté bien introducido. Por ejemplo: Si introduce un float con un punto en lugar de con una coma, debe mostrar “Error de formato”. Hasta que el usuario/a no introduce un float bien formado debe seguir pidiendo el dato.
+
+Todos los métodos reciben un String con el mensaje que se quiere mostrar al usuario, por ejemplo: “Introduce tu edad”, y devuelven el dato oportuno introducido por el usuario en cada método, por ejemplo: uno byte con la edad del usuario/a.
+
+Métodos a implantar capturando la excepción de la clase InputMismatchException:
+
+public static byte leerByte(String mensaje);
+
+public static int leerInt(String mensaje);
+
+public static float leerFloat(String mensaje);
+
+public static double leerDouble(String mensaje);
+
+Métodos a implantar capturando la excepción de la clase Exception:
+
+public static char leerChar(String mensaje);
+
+public static String leerString(String mensaje);
+
+public static boolean leerSiNo(String mensaje), si el usuario introduce “s”, devuelve “true”, si el usuario introduce “n”, devuelve “false”.
+
+*/
