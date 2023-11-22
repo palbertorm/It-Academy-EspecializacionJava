53b3f7374f1725182a22d8228e6c64f703850c28
pauloalberto <palbertorm35@gmail.com>
2023-11-22 17:49:02 +0100

    wip exec1 tasca 3

--- Tasca_3/S1_03_N1_Exe1/src/Main.java
+++ Tasca_3/S1_03_N1_Exe1/src/Main.java
@@ -1,21 +1,30 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         String nomeMes;
-        ArrayList<Month> months = new ArrayList<Month>();
+        List<Month> months = new ArrayList<Month>();
 
         for (int i = 1; i < 12; i++) {
             System.out.println(" Introduzca el Month " + i);
-            nomeMes = input.nextLine();
+            nomeMes = input.nextLine().toLowerCase();
             months.add(new Month(nomeMes));
         }
+        if (months.contains("agost")){
+            months.remove("agost");
+        }
         Collections.sort(months);
-      //  System.out.println(months.get());
+
+        System.out.println("meses ordenados");
         for (Month month: months) {
+            System.out.println(month.getName());
+        }
+
+        months.set(1, new Month("Agost"));// inserting new object in the position
+
+        for (Month month: months) {
+
             System.out.println(month.getName());
         }
     }
