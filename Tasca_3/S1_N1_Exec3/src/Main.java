c00d887a7188d51d100f36cf29a08d7b2d47de86
pauloalberto <palbertorm35@gmail.com>
2023-11-24 19:59:20 +0100

    add coubntries

--- Tasca_3/S1_N1_Exec3/src/Main.java
+++ Tasca_3/S1_N1_Exec3/src/Main.java
@@ -3,38 +3,39 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.sql.Array;
+import java.util.*;
+
+import static java.lang.Math.random;
 
 public class Main {
+        static Map<String, String> map = new HashMap<String, String>();
+        static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
+        String nombreUser= "";
+        String capitalUser= "";
+
+
+        System.out.println("Dime tu nombre");
+        nombreUser = input.next();
+        System.out.println("Hola  "+ nombreUser);
+        System.out.println("introduce el nombre de la capital " + lerArquivo());
+        capitalUser = input.next().toLowerCase();
+        addCapital(capitalUser);
+    }
+    public static String lerArquivo(){
         File countries = new File("../countries.txt");
-        Map<String, String> map = new HashMap<String, String>();
         Scanner myReader = null;
         try {
             //BufferedReader reader = new BufferedReader(new FileReader( countries));
             myReader = new Scanner(countries);
+        String line = myReader.nextLine().toLowerCase();
             while (myReader.hasNextLine()) {
-                String[] data = myReader.nextLine().split("\\s");
+                String[] data = myReader.nextLine().toLowerCase().split("\\s");
                 String key = data[0].trim();
                 String value = data[1];
                 map.put(key, value);
-                System.out.println(data.length);
-
-                }
-
-
-//           while (reader.ready()) {
-//               String data = reader.readLine();
-//               System.out.println(data);
-//               data.getBytes(StandardCharsets.UTF_8);
-//               map.put(data, String.valueOf(data.length()));
-//                String data = myReader.nextLine();
-//                map.put(data , data.substring(0, data.length()));
-//           }
-
+            }
         } catch (IOException e) {
             System.out.println("Error 35: " + e.getMessage());
             e.printStackTrace();
@@ -42,6 +43,36 @@ public class Main {
         finally {
             myReader.close();
         }
-        System.out.println("mapeio" + map.get("Spain"));
+        Object[] countryKey = map.keySet().toArray();
+        Object key = countryKey[new Random().nextInt(countryKey.length)];
+
+        return (String) key;
+    }
+    public static void addCapital(String nameCapital) {
+        HashSet<String> capitals = new HashSet<String>();
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(" de " + lerArquivo());
+            nameCapital = input.next().toLowerCase();
+            if (map.containsValue(nameCapital)) {
+
+            capitals.add(nameCapital);
+            };
+            System.out.println(capitals.size());
+        }
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter("../classificacion.txt"));
+            for (String name : capitals){
+                writer.write("\n " + name);
+
+            }
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }finally {
+        }
+
+
     }
 }
