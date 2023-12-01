db9bb183ba1bdbd2088ca2f6f82a9af043bd1cb4
Alberto Moura <palbertorm35@gmail.com>
2023-12-01 02:51:40 +0100

    sort directory

--- Tasca_5/S1_05_N1_Exe1/src/main/java/org/example/ListaDirectorio.java
+++ Tasca_5/S1_05_N1_Exe1/src/main/java/org/example/ListaDirectorio.java
@@ -1,24 +1,25 @@
 package org.example;
-
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.comparator.CompositeFileComparator;
+import org.apache.commons.io.comparator.NameFileComparator;
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
-import java.util.Objects;
 
-public class ListaDirectorio implements Comparator<File> {
+public class ListaDirectorio {
 
     public void ordenarDirectorio(String directorioName){
-        File directorio = new File(directorioName);
-//        Path directorio = Path.of(directorioName);
-//        List<File> archivos = List.of(Objects.requireNonNull(directorio.listFiles()));
-        List<File> archivos = List.of(directorio.listFiles());
-        Collections.sort(archivos, this);
+            File directorio = new File(directorioName);
+            File[] archivos = directorio.listFiles();
+        try{
+            CompositeFileComparator comparator = new CompositeFileComparator();
+            comparator.sort(archivos);
+
+        }catch (SecurityException e) {
+            e.printStackTrace();
+        }
     }
-    @Override
+ /*   @Override
     public int compare(File file1, File file2) {
         return file1.getName().toLowerCase().compareTo(file2.getName().toLowerCase());
-    }
+    }*/
 }
