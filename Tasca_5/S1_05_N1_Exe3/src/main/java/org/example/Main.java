b32c1f5d6b98e80dce1de25bebc1d47f3dcca9ce
pauloalberto <palbertorm35@gmail.com>
2023-12-05 15:10:15 +0100

    ejercicio 3 nvl1 tasca 5

--- Tasca_5/S1_05_N1_Exe3/src/main/java/org/example/Main.java
+++ Tasca_5/S1_05_N1_Exe3/src/main/java/org/example/Main.java
@@ -2,9 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Starting");
         String dir = "/Users/pauloalberto/Documents/It-Academy-EspecializacionJava";
-        ListarDirectorio listarDirectorio = new ListarDirectorio();
-        listarDirectorio.ordenarDirectorio(dir);
+        WriteNewDir writeNewDir = new WriteNewDir();
+        writeNewDir.ordenarDirectorio(dir);
     }
 }
