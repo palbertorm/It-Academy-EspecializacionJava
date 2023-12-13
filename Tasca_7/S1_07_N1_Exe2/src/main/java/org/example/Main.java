325ef3e072cdaa4a830f22efe4531006535331a5
pauloalberto <palbertorm35@gmail.com>
2023-12-13 12:03:16 +0100

    ejercicio 2 tasca 7 nivel 1

---
+++ Tasca_7/S1_07_N1_Exe2/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        TrabajadorOnline trabajadorOnline = new TrabajadorOnline("Helio","Gracie", 15);
+        trabajadorOnline.souHorasBaja(100, 60);
+        TrabajadorPresencial trabajadorPresencial = new TrabajadorPresencial("Juan","Saboia", 12);
+        TrabajadorPresencial.setGasolina(285);
+        System.out.println(" sueldo trabajador online " + trabajadorOnline.calcularSou(170));
+        System.out.println(" sueldo trabajador presencial " + trabajadorPresencial.calcularSou(165.5));
+        System.out.println(" sueldo con baja y metodo deprecated de trabajador online " + trabajadorOnline.souHorasBaja(100, 60));
+        System.out.println(" sueldo con baja y metodo deprecated de trabajador presencial " + trabajadorPresencial.souHorasBaja(100, 60));
+    }
+}
---
+++ Tasca_7/S1_07_N1_Exe2/src/main/java/org/example/TrabajadorPresencial.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class TrabajadorPresencial extends Trabajador{
+    private static int gasolina;
+    public TrabajadorPresencial(String nombre, String apellido, double precioHora) {
+        super(nombre, apellido, precioHora);
+    }
+
+    public static int getGasolina() {
+        return gasolina;
+    }
+
+    public static void setGasolina(int gasolina) {
+        TrabajadorPresencial.gasolina = gasolina;
+    }
+
+    @Override
+    public double calcularSou(double horasTrabajadaMes) {
+        return getPrecioHora() * horasTrabajadaMes + gasolina;
+    }
+    public int souHorasBaja(double horasTrabajada, double horasBaja) {
+
+        return (int) (getPrecioHora() * (horasTrabajada - horasBaja)+ gasolina);
+    }
+}
