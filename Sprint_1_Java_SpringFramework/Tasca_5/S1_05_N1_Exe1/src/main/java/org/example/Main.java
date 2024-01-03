6653ea1486f4d7f4d2b202e70789c069b67c5cad
pauloalberto <palbertorm35@gmail.com>
2024-01-03 00:40:51 +0000

    sprint 2 tasca 1

--- Sprint_1_Java_SpringFramework/Tasca_5/S1_05_N1_Exe1/src/main/java/org/example/Main.java
+++ Sprint_1_Java_SpringFramework/Tasca_5/S1_05_N1_Exe1/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting");
-        String dir = "/Users/pauloalberto/Downloads";
+        String dir = "../It-Academy-EspecializacionJava";
         ListaDirectorio listaDirectorio = new ListaDirectorio();
         listaDirectorio.ordenarDirectorio(dir);
     }
