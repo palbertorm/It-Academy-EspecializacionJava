003883a88ee8fb16a450d18675776914a948780a
pauloalberto <palbertorm35@gmail.com>
2024-01-22 16:27:10 +0100

    correct sprint 1

--- Sprint_1_Java_SpringFramework/Tasca_5/S1_05_N1_Exe2/src/main/java/org/example/Main.java
+++ Sprint_1_Java_SpringFramework/Tasca_5/S1_05_N1_Exe2/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting");
-        String dir = "/Users/pauloalberto/Documents/It-Academy-EspecializacionJava";
+        String dir = ".././";
         ListarDirectorio listarDirectorio = new ListarDirectorio();
         listarDirectorio.ordenarDirectorio(dir);
     }
--- Sprint_1_Java_SpringFramework/Tasca_5/S1_05_N1_Exe3/src/main/java/org/example/Main.java
+++ Sprint_1_Java_SpringFramework/Tasca_5/S1_05_N1_Exe3/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String dir = "/Users/pauloalberto/Documents/It-Academy-EspecializacionJava";
+        String dir = ".././";
         WriteNewDir writeNewDir = new WriteNewDir();
         writeNewDir.ordenarDirectorio(dir);
     }
