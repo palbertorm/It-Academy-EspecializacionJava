24ac9318f99233d5f264db653ecdbb204278458e
pauloalberto <palbertorm35@gmail.com>
2024-03-04 14:08:22 +0000

    organized folders

---
+++ Sprint_4_SpringFrameWork/Tasca_2/NIVEL_3/S04T02N03PauloAlberto/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n03/S04T02N03PauloAlberto/repository/FruitaRepository.java
@@ -0,0 +1,10 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n03.S04T02N03PauloAlberto.repository;
+
+import cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n03.S04T02N03PauloAlberto.model.Fruita;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface FruitaRepository extends MongoRepository<Fruita, String> {
+    List<Fruita> findByName(String name);
+}
